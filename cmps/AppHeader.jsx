6773e69f92e72f15df9f20b4d047e27913283173
Or Ben Yair <orbenyair@Or-Ben-Yair-VMFJKXVVFV.local>
2024-11-22 18:30:22 +0200

    The first Commit

---
+++ cmps/AppHeader.jsx
@@ -0,0 +1,11 @@
+
+export function AppHeader() {
+
+    return (
+        <header className="app-header full main-layout">
+            <section className="header-container">
+                <h1>Miss Books</h1>
+            </section>
+        </header>
+    )
+}
---
+++ cmps/Home.jsx
@@ -0,0 +1,10 @@
+const { useState, useEffect } = React
+
+export function Home() {
+    return (
+        <section className="home">
+            <h2>Welcome to Miss Books webpage</h2>
+       </section>
+    )
+}
+
