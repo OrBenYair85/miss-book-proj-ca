e58d859c256956bb4a2fdd819a6a2aefbe42bd11
Or Ben Yair <orbenyair@Or-Ben-Yair-VMFJKXVVFV.local>
2024-11-28 13:05:32 +0200

    adding books preview + books style

--- cmps/BooksComp/BookPreview.jsx
+++ cmps/BooksComp/BookPreview.jsx
@@ -3,14 +3,18 @@ import { bookService } from "../../services/bookService.js"
 
 export function BooksPreview(){
 
+    const demoBook = {id:'1', title:'Demo Book', author:'Or Ben Yair'}
 
-    
 
 
     return (
         <section className="book-preview-container">
             <section className="book-preview">
-
+                <h1>{demoBook.title}</h1>
+                <img src={`./assets/img/BooksImages/${demoBook.id}.jpg`} />
+                <h4>{demoBook.author}</h4>
+                <button className="book-button">Details</button>
+                <button className="book-button">Remove</button>
             </section> 
 
         </section>
--- cmps/BooksIndex.jsx
+++ cmps/BooksIndex.jsx
@@ -1,11 +1,12 @@
 import { BooksPreview } from "./BooksComp/BookPreview.jsx"
 
+
 export function BooksIndex(){
     return (
         <section className="book-index-container">
             <section className="book-index">
             <h1>Our Books</h1>
-            
+            <BooksPreview />
             </section>
 
         </section>
