9116e502cf1412c4fc6a63de4249c5c7957d342c
Or Ben Yair <orbenyair@Or-Ben-Yair-VMFJKXVVFV.local>
2024-11-30 18:24:17 +0200

    Adding price to the books + start the work of book filter

---
+++ cmps/BooksComp/BookFilter.jsx
@@ -0,0 +1,10 @@
+import { bookService } from "../../services/bookService.js";
+
+
+const {useState, useReff, useEffect} = React
+
+export function BookFilter({defaultFilter,onSet}){
+
+const [filterType, setFilterType] = useState()
+
+}
